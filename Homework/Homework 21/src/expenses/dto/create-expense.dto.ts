95882d2b3dd52282c0276d32dd01585f1b9827b7
GeorgeShani <shanidze.geo2004@gmail.com>
2026-07-21 17:58:03 +0400

    feat: add class-transformer and class-validator dependencies; enhance DTO validation in user and expense models

--- Homework/Homework 21/src/expenses/dto/create-expense.dto.ts
+++ Homework/Homework 21/src/expenses/dto/create-expense.dto.ts
@@ -1,6 +1,18 @@
+import { IsInt, IsNotEmpty, IsPositive, IsString } from 'class-validator';
+
 export class CreateExpenseDto {
+  @IsString()
+  @IsNotEmpty()
   category!: string;
+
+  @IsString()
+  @IsNotEmpty()
   productName!: string;
+
+  @IsInt()
+  @IsPositive()
   quantity!: number;
+
+  @IsPositive()
   price!: number;
 }
